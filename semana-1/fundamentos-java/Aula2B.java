2b549ba2609dcc92cc909a7de69d3eb1d8c0c025
jabuDev <jabulanisifunda@gmail.com>
2025-11-18 22:50:02 +0200

    Exercicio b

---
+++ semana-1/fundamentos-java/Aula2B.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Aula2B {
+
+    public static void main(String[] args) {
+        //declaracao de variaveis
+        int anodenascimento;
+        String nome;
+        int anoatual = 2025;
+        int idade;
+        String situacaodapessoa;
+        Scanner teclado = new Scanner(System.in);
+
+        //receber dados por teclado
+        System.out.println("digite o nome");
+        nome = teclado.nextLine();
+        System.out.println("digite o ano de nascimento");
+        anodenascimento = teclado.nextInt();
+
+
+idade = anoatual - anodenascimento;
+
+
+            if (idade>=18)  {
+                situacaodapessoa = "pode votar";
+            
+            } else {
+                situacaodapessoa = "nao pode votar";
+            }
+
+
+System.out.println("O "+nome+" tem "+ idade +" anos de idade e ele/a " + situacaodapessoa);
+
+    }
+}
